56c28dab2c4912a845949fcaa906fbdabc67891c
Taras Kalapun <t.kalapun@gmail.com>
2011-02-24 14:18:04 +0200

    Added comment to Parser how it was in console

--- inOrder/Parser.h
+++ inOrder/Parser.h
@@ -6,6 +6,36 @@
 //  Copyright 2010 Ciklum. All rights reserved.
 //
 
+/*
+ 
+ Console application was like this:
+ 
+ for (int i=0; i<argc; i++) {
+ NSString *argString = [NSString stringWithCString:argv[i] encoding:NSUTF8StringEncoding];
+ NSLog(@"incoming parameter: %d, %@", i, argString);
+ 
+ }
+ 
+ 
+ if (argc < 2) {
+ NSLog(@"Please provide valid pbproj file");
+ return 0;
+ }
+ 
+ NSString *dataFile = [NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding];
+ 
+ Parser *p = [[Parser alloc] init];
+ 
+ [p parseFile:dataFile];
+ [p populateFilesAndGroups];
+ 
+ [p printPaths];
+ NSLog(@"result %@", p.files);
+ 
+ [p release];
+ 
+ */
+
 #import <Foundation/Foundation.h>
 
 
