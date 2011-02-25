10d378dfe30c469c2d9ead65cea1847ce14aba80
Taras Kalapun <t.kalapun@gmail.com>
2011-02-26 00:43:25 +0200

    Added DropZoneView, dropZone image, added ability to catch dropped files on dropZone & on dock icon

---
+++ inOrder/DropZoneView.h
@@ -0,0 +1,17 @@
+//
+//  DropZoneView.h
+//  inOrder
+//
+//  Created by Taras Kalapun on 26.02.11.
+//  Copyright 2011 Ciklum. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+
+@interface DropZoneView : NSImageView {
+@private
+    
+}
+
+@end
--- inOrder/inOrderAppDelegate.m
+++ inOrder/inOrderAppDelegate.m
@@ -10,11 +10,46 @@
 
 @implementation inOrderAppDelegate
 
-@synthesize window;
+@synthesize window, dropZone;
+
+- (void)dealloc {
+    self.window = nil;
+    self.dropZone = nil;
+    [super dealloc];
+}
+
+#pragma mark -
+#pragma mark Application delegates
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    
+
+    
 }
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
+    return YES;
+}
+
+#pragma mark -
+#pragma mark Dragging delegates
+
+// Handle a file dropped on the dock icon
+- (BOOL)application:(NSApplication *)sender openFile:(NSString *)path
+{
+    
+    // !! Do something here with the file path !!
+    NSLog(@"Dropped file: %@ from %@", path, sender);
+    
+    
+    //NSImage *icon = [[NSWorkspace sharedWorkspace] iconForFile:path];
+	//NSSize newSize = NSMakeSize([image frame].size.width, [image frame].size.height);
+	
+    
+    return YES;
+}
+
 
 @end
